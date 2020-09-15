c61afa72d530cb271b698604bbfc7b3e695f3ec1
Yaroslavhavryliuk <60150454+Yaroslavhavryliuk@users.noreply.github.com>
2020-09-15 18:09:30 +0300

    Add files via upload

---
+++ LeetCode4.cpp
@@ -0,0 +1,54 @@
+﻿
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void choicesSort(vector<int>& arrayPtr, int length_array);
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
+    {
+        for (int i = 0; i < nums2.size(); i++)
+        {
+            nums1.push_back(nums2[i]);
+        }
+        choicesSort(nums1, nums1.size());
+        if ((nums1.size() % 2) != 0)
+        {
+            return (double)nums1[nums1.size() / 2];
+        }
+        else
+        {
+            return (double)(nums1[nums1.size() / 2] + nums1[(nums1.size() / 2) - 1]) / 2;
+        }
+    }
+};
+
+void choicesSort(vector<int>& arrayPtr, int length_array) // сортировка выбором
+{
+    for (int repeat_counter = 0; repeat_counter < length_array; repeat_counter++)
+    {
+        int temp = arrayPtr[0]; // временная переменная для хранения значения перестановки
+        for (int element_counter = repeat_counter + 1; element_counter < length_array; element_counter++)
+        {
+            if (arrayPtr[repeat_counter] > arrayPtr[element_counter])
+            {
+                temp = arrayPtr[repeat_counter];
+                arrayPtr[repeat_counter] = arrayPtr[element_counter];
+                arrayPtr[element_counter] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    vector<int> num1 = { 3, 11, 8, 13 }, num2 = { 5, 4, 9 };
+    choicesSort(num1, num1.size());
+    choicesSort(num2, num2.size());
+    Solution sl;
+    cout << sl.findMedianSortedArrays(num1, num2) << endl;
+    return 0;
+}
+
---
+++ LeetCode5.cpp
@@ -0,0 +1,103 @@
+﻿
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+class Palindrome;
+class Solution;
+
+class Palindrome
+{
+    friend Solution;
+public:
+    int getstrSize()
+    {
+        return strLong(str);
+    }
+private:
+    vector<char>str;
+    int strSize;
+    int strLong(vector<char>& str)
+    {
+        return str.size();
+    }
+};
+
+class Solution 
+{
+public:
+    vector<char> longestPalindrome(string s)
+    {
+        if (s.length() == 0)
+        {
+            return {};
+        }
+        vector<Palindrome> palMax;
+        for (int i = 0; i < s.length(); i++)
+        {
+            for (int j = s.length() - 1; j >= i; j--)
+            {
+                bool isPal = true;
+                int count = 0;
+                for (int k = i; k <= j; k++)
+                {
+                    
+                    if (s[k] == s[j - count])
+                    {
+                        count++;
+                        continue;
+                    }
+                    else
+                    {
+                        isPal = false;
+                        break;
+                    }
+                    
+                }
+                if (isPal)
+                {
+                    Palindrome sToPalMax;
+                    for (int l = i; l <= j; l++)
+                    {
+                        sToPalMax.str.push_back(s[l]);
+                    }
+                    palMax.push_back(sToPalMax);
+                }
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < palMax.size(); i++)
+        {
+            if (palMax[i].getstrSize() > max)
+            {
+                max = palMax[i].getstrSize();
+            }
+        }
+        for (int i = 0; i < palMax.size(); i++)
+        {
+            if (palMax[i].getstrSize() == max)
+            {
+                return palMax[i].str;
+            }
+        }
+        
+    }
+};
+
+
+
+int main()
+{
+    Solution sl;
+    vector<char> answ;
+    answ=sl.longestPalindrome("msalaskr");
+    for (int i = 0; i < answ.size(); i++)
+    {
+        cout << answ[i];
+    }
+    return 0;
+}
+
